18f1fc3dbe2966037b2166495f2a12927de5a6ac
Gyan kashyap <88448003+kashgyan@users.noreply.github.com>
2023-06-02 01:23:36 +0530

    Update abundant number .java

--- getting started /abundant number .java
+++ getting started /abundant number .java
@@ -1,1 +1,23 @@
 
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int p = scn.nextInt();
+        int temp = p;
+        int count = 0;
+        for(int i = 1;i<temp ; i++){
+            if(temp % i == 0){
+                count = count +i;
+            }
+        }
+        System.out.println("count "+ count);
+        
+        if(count > p){
+            System.out.println("abundant  number");
+        }else{
+            System.out.println("Not abundant number");
+        }
+    }
+}
