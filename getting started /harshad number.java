46911fb8614b5d6476aa641c18d8612be5e9287c
Gyan kashyap <88448003+kashgyan@users.noreply.github.com>
2023-06-02 01:06:14 +0530

    Update harshad number.java

--- getting started /harshad number.java
+++ getting started /harshad number.java
@@ -1,1 +1,21 @@
 
+import java.util.*;
+class HelloWorld {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int p = scn.nextInt();
+        int temp = p;
+        int count = 0;
+        while(temp>0){
+            int rem = temp%10;
+            temp = temp /10;
+            count = count + rem;
+        }
+        if(p % count == 0){
+            System.out.println("harshad number");
+        }else{
+            System.out.println("NOt harshad number");
+        }
+    }
+}
